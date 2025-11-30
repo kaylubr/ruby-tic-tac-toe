d4d7da92af4c63f17c08c8d66aae177795408763
kaylubr <kylebreyes092005@gmail.com>
2025-11-30 12:58:28 +0800

    Feat: Add Cell Class

--- lib/Board.rb
+++ lib/Board.rb
@@ -1,7 +1,17 @@
+require_relative 'Cell'
+
 class Board
   attr_accessor :game_board
 
   def initialize
     @game_board = []
+  end
+
+  def create_board
+    3.times do
+      3.times do
+        game_board << [Cell.new('O')]
+      end
+    end
   end
 end
---
+++ main.rb
@@ -0,0 +1,6 @@
+require_relative 'lib/Board'
+
+board = Board.new
+board.create_board
+
+p board.game_board
